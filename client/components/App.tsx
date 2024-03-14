abd386b668b447b84e250064be50657f2c642822
Erick Silva <erickmsil32@gmail.com>
2024-03-14 23:41:15 +1300

    Added Play/Pause + Next song button. Background changes consequently.

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,29 +2,31 @@ import { useState } from 'react'
 import { getGreeting } from '../apiClient.ts'
 import { useQuery } from '@tanstack/react-query'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+// Stuff that was already here VVV
+// const App = () => {
+//   const [count, setCount] = useState(0)
 
-  const {
-    data: greeting,
-    isError,
-    isPending,
-  } = useQuery({ queryKey: ['greeting', count], queryFn: getGreeting })
+//   const {
+//     data: greeting,
+//     isError,
+//     isPending,
+//   } = useQuery({ queryKey: ['greeting', count], queryFn: getGreeting })
 
-  if (isPending) return <p>Loading...</p>
+//   if (isPending) return <p>Loading...</p>
 
-  return (
-    <>
-      {count}
-      <h1 className="text-3xl font-bold underline">{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       {count}
+//       <h1 className="text-3xl font-bold underline">{greeting}</h1>
+//       {isError && (
+//         <p style={{ color: 'red' }}>
+//           There was an error retrieving the greeting.
+//         </p>
+//       )}
+//       <button onClick={() => setCount(count + 1)}>Click</button>
+//     </>
+//   )
+// }
+// End of stuff that was already here
 
 export default App
