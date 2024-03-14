37533166980b8757238f2d72cc9025e8d8759a94
tempo <tempo@PJ-766262-K01>
2024-03-15 09:56:54 +1300

    inprogress

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -25,6 +25,16 @@ const App = () => {
     <>
     <h1 className="text-3xl font-bold underline">{zenQuote[0].q}</h1>
     <button onClick={() => setMode('fbMom')}>wine time 🍷</button>
+    <script src="./client/index.tsx" type="module"></script>
+    <div id="container">
+      <button id="playButton">⏸️</button>
+      <button id="nextButton">⏭️</button>
+      <audio id="audioPlayer" autoplay>
+        <source src="https://audio.jukehost.co.uk/Sa3fJ2syKJVdIjHObgNLMy5BvCvtYDiI" type="audio/mpeg" />
+        
+      </audio>
+    </div>
+    <script src="./Song&Ima.js"></script>
     </>
     }
 
