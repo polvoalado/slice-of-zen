38b68855b5f8f85c1a09d8b29b9a200bc0d10e94
tempo <tempo@PJ-766262-K01>
2024-03-15 10:24:55 +1300

    fixing stuff

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
-import { getRandomQuote } from '../apiClient.ts'
-import { useQuery } from '@tanstack/react-query'
 import FbMom from './FbMom.tsx'
+import Zen from './Zen.tsx'
 
 const App = () => {
   const [mode, setMode] = useState('zen')
 
-  const {
-    data: zenQuote,
-    isError,
-    isPending,
-  } = useQuery({ queryKey: ['zenQuote'], queryFn: getRandomQuote })
+
 
 
 //   if (isPending) return <p>Loading...</p>
@@ -23,27 +18,9 @@ const App = () => {
     </>
     :
     <>
-    <h1 className="text-3xl font-bold underline">{zenQuote[0].q}</h1>
-    <button onClick={() => setMode('fbMom')}>wine time 🍷</button>
-    <script src="./client/index.tsx" type="module"></script>
-    <div id="container">
-      <button id="playButton">⏸️</button>
-      <button id="nextButton">⏭️</button>
-      <audio id="audioPlayer" autoplay>
-        <source src="https://audio.jukehost.co.uk/Sa3fJ2syKJVdIjHObgNLMy5BvCvtYDiI" type="audio/mpeg" />
-        
-      </audio>
-    </div>
-    <script src="./Song&Ima.js"></script>
+      <Zen setMode={setMode}/>
     </>
     }
-
-{isError && (
-      <p style={{ color: 'red' }}>
-        There was an error retrieving the greeting.
-      </p>
-    )}
-
     </>
   )
 }
---
+++ client/components/Zen.tsx
@@ -0,0 +1,35 @@
+import { getRandomQuote } from '../apiClient.ts'
+import { useQuery } from '@tanstack/react-query'
+
+function Zen({ setMode }) {
+
+    const {
+        data: zenQuote,
+        isError,
+        isPending,
+      } = useQuery({ queryKey: ['zenQuote'], queryFn: getRandomQuote })
+
+    return (
+      <>
+<h1 className="text-3xl font-bold underline">{zenQuote[0].q}</h1>
+    <button onClick={() => setMode('fbMom')}>wine time 🍷</button>
+    {/* <script src="../index.tsx" type="module"></script> */}
+    <div>
+      <button id="playButton">⏸️</button>
+      <button id="nextButton">⏭️</button>
+      <audio id="audioPlayer">
+        <source src="https://audio.jukehost.co.uk/Sa3fJ2syKJVdIjHObgNLMy5BvCvtYDiI" type="audio/mpeg" />
+      </audio>
+    </div>
+    <script src="/Song&Ima.ts"></script>
+    {isError && (
+      <p style={{ color: 'red' }}>
+        There was an error retrieving the greeting.
+      </p>
+    )}
+      </>
+    )
+  }
+  
+  export default Zen
+  
